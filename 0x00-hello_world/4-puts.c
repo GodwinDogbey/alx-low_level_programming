19cc154af45bd0323517295b4893487e1638b10f
GodwinDogbey <godwindogbey95@gmail.com>
2023-02-16 19:00:05 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 
 /**
-*main - Entry point
-*Return: Always 0 (success)
+*main - prints "Programming is like building a multilingual puzzle, followed by a new line. 
+*Return: Always 0.
 */
 int main(void)
 {
    
-       	puts("Programming is like building a multilingual puzzle")
+       	puts("\"Programming is like building a multilingual puzzle")
         return (0);
 
 }	    
